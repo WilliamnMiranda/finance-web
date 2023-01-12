1b096e58c3c83aab1c7a467ae0c74752aa3b0b58
william nascimento <williamnnmiranda@gmail.com>
2023-01-12 12:13:19 -0300

    :recycle: refactor: login function passed to arrow function

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,12 +1,10 @@
 import userServices from "../services/user"
 import { ILogin } from "../types/user"
-
+import React from 'react'
 const useUser = () => {
-  const login = async (data: ILogin) => {
-    const user = await userServices.login(data)
-    console.log(user)
-  }
 
+  const login = async (data: ILogin) => await userServices.login(data)
+  
   return {
     login
   }
